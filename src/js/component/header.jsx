c26fa53c15a3ddde6b60fadc9fb3cf8d1cc59580
Samantha McCabe <156986930+SamanthaMcCabe2104@users.noreply.github.com>
2024-06-05 22:14:04 +0000

    code reviewed and styled

--- src/js/component/header.jsx
+++ src/js/component/header.jsx
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 const Header = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center pt-5">to do</h1>
+			<h1 className="text-center pt-5">To do!</h1>
         </div>
     );
 };
